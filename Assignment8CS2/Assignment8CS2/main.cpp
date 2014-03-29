565c98757b7245a26163adcb6e63e1fe59a24cfe
Jen <jenniferballing@gmail.com>
2014-03-29 10:28:09 -0600

    receiving info from file for maze

--- Assignment8CS2/Assignment8CS2/main.cpp
+++ Assignment8CS2/Assignment8CS2/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int CountCannonBalls( int levels);
 void PrintBinary (int num);
 int ComputeSumOfDigits (int num);
-void MazeSolver();
-void FindPath(int r, int c);
+void MazeSolver(int r, int c);
+bool findPath(int r, int c);
 void North(int r, int c);
 void South(int r, int c);
 void East(int r, int c);
@@ -33,7 +33,7 @@ int main ()
     cout<<"Should be 28: "<<ComputeSumOfDigits(5698)<<endl;
     cout<<"Should be 18: "<<ComputeSumOfDigits(963)<<endl;
 
-    MazeSolver();
+    //MazeSolver();
 
 
     system("pause");
@@ -77,7 +77,7 @@ int ComputeSumOfDigits (int num)
     }
     return total;
 }
-void MazeSolver()
+/*void MazeSolver(int r, int c)
 {
     //read in first line of file for bounds
     int numRows=0, numCols=0, startRows=0, startCols=0;
@@ -98,14 +98,77 @@ void MazeSolver()
     st>>startRows;
     st>>startCols;
 
+    //checker from online...that takes current location and had file info
+    char charAt;
+    if(r>numRows || r<0 || c<0 ||c>numCols) return false;
+    if(charAt=='t') return true;
+
+
+
+}*/
+bool findPath (int r, int c)
+{    
+    int numRows, numCols;
+    string RAndC, line;
+    string strArr[20];
+    fstream fin("maze1.txt");    
+    getline(fin, RAndC);
+
+    stringstream ss;
+    ss.str(RAndC);
+    ss>>numRows;
+    ss>>numCols;
+
+    for(int i=0; i<numRows; i++)
+    {
+        getline(fin,line);
+        strArr[i]=line;
+    }
+    return 0;
     
 
-    //checker from online...that takes current location and had file info
+    /*
 
+    char tempChar;
+
+    char* TwoD [50];
+    fstream file("maze1.txt");
+    for(int i=0; i<numRows; i++)
+    {
+        for(int j=0; j<numCols; j++)
+        {
+            file>>tempChar;
+            TwoD[i][j]=tempChar;
+        }
+    }
+    //getline (file, 
+    
+    char charAt;
+    if(charAt==' ') return false;
+    if(charAt=='t') return true;
+    if(charAt=='X') return false;
+    charAt='S';//file io
+    if(findPath(North(r, c)==true) return true;
+    if(findPath(South(r,c)== true) return true;
+    if(findPath(East(r, c)==true) return true;
+    if(findPath(West(r, c)==true) return true;
+    charAt='.';//file io*/
 
 }
-void FindPath(int r, int c)
+void North(int r, int c)
 {
-    if(
+    c-=1;
+}
+void South(int r, int c)
+{
+    c+=1;
+}
+void East(int r, int c)
+{
+    r+=1;
+}
+void West(int r, int c)
+{
+    r-=1;
 }
 
